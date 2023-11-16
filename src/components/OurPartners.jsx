387730a8d7120f75265fd4306a73344f7f21c78f
MizanSafin <drmizanurrahman452@gmail.com>
2023-11-16 16:24:11 +0600

    1st time created

---
+++ src/components/OurPartners.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function OurPartners() {
+  return (
+    <div className="ourPartner">
+      <img src="images/partner_images/google.png" alt="" />
+      <img src="images/partner_images/Trello.png" alt="" />
+      <img src="images/partner_images/monday.png" alt="" />
+      <img src="images/partner_images/Notion.png" alt="" />
+      <img src="images/partner_images/Slack.png" alt="" />
+    </div>
+  );
+}
+
+export default OurPartners;
